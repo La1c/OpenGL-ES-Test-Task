60e5c6ccfc651aba2cb13949d2aab855f9fc26eb
Vladimir Ageev <vladimir.ageev@me.com>
2017-11-29 15:53:53 +0300

    Initial Commit

---
+++ OpenGL_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenGL_Test
+//
+//  Created by Vladimir on 29.11.2017.
+//  Copyright © 2017 Vladimir Ageev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
